706e72f4f838597330c5fb0334260f4404f7185c
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2022-07-05 13:42:20 +0300

    odev9.sql updated.

--- odev9.sql
+++ odev9.sql
@@ -7,5 +7,5 @@ SELECT customer.first_name, customer.last_name, payment.amount FROM customer
 INNER JOIN payment ON customer.customer_id = payment.customer_id;
 
 -- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
-SELECT customer.first_name, customer.last_name FROM customer
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
 INNER JOIN rental ON customer.customer_id = rental.customer_id;
